53ddfabadbd87fc67b41558382b11b8c16f7a806
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-04-04 14:31:14 -0700

    added reset to scss

--- public/partials/global/head.php
+++ public/partials/global/head.php
@@ -3,6 +3,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forward - Meet and Connect with Local Mentors</title>
-    <link rel="stylesheet" href="<?php echo get_public_url('/styles/css/reset.css'); ?>">
-    <link rel="stylesheet" href="<?php echo get_public_url('/styles/css/main.css'); ?>">
+    <link rel="stylesheet" href="<?php // echo get_public_url('/styles/css/reset.css'); ?>">
+    <link rel="stylesheet" href="<?php echo get_public_url('/styles/css/main.css'); ?>?v=1">
 </head>
