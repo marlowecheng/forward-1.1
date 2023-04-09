61d3cf4644e2d2352396e8cbf5b7ccb2c4831e7b
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-04-08 18:56:30 -0700

    css cache busting

--- public/partials/global/head.php
+++ public/partials/global/head.php
@@ -3,5 +3,5 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forward - Meet and Connect with Local Mentors</title>
-    <link rel="stylesheet" href="<?php echo get_public_url('/styles/css/main.min.css'); ?>?v=1">
+    <link rel="stylesheet" href="<?php echo get_public_url('/styles/css/main.min.css'); ?>?v=1.1">
 </head>
