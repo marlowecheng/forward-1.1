d26140b2368a840358132d7d24a6710725632f3d
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-04-08 20:41:17 -0700

    community thread pg heading

--- public/pages/community-thread.php
+++ public/pages/community-thread.php
@@ -12,9 +12,21 @@
             <?php
                 require(get_path('public/partials/components/side-nav.php'));
             ?>
-            <section class="no-margin active-container thread-content">
-                <div class="">
-
+            <section class="no-margin active-container thread-main">
+                <div class="btn-bwd"><a href="" class="d-flex">
+                    <i data-feather="arrow-left"></i>Go Back
+                </a></div>
+                <div class="thread-content">
+                    <div class="thread-head">
+                        <div class="head-info">
+                            <img src="" alt="" class="avatar">
+                            <p >Posted by @lilybrowndesigns 9 hours ago</p>
+                        </div>
+                        <div class="head-icons">
+                            <i data-feather="bookmark"></i>
+                            <i data-feather="share-2"></i>
+                        </div>
+                    </div>
                 </div>
             </section>
             <section class="no-margin thread-comments">
