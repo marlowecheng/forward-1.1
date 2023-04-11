a856cf30a6c14236e2114588f9e5d33e6a60ef5b
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-04-11 16:18:47 -0700

    header updated to reflect design doc

--- public/partials/global/header.php
+++ public/partials/global/header.php
@@ -10,12 +10,11 @@
                 <ul class="global-nav">
                     <li class="flex-left"><a href="#" class="" title="Go to Community Hub">Community</a></li>
                     <li class="flex-left"><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" class="" title="Project Showcase">Showcase</a></li>
-                    <li><a href="<?php echo get_public_url('/pages/mentor-list.php'); ?>" class="header-btn" title="See List of Mentors">Mentors</a></li>
+                    <li class="nav-btn"><a href="<?php echo get_public_url('/pages/mentor-list.php'); ?>" title="See List of Mentors">Mentors</a></li>
                     <img class="notification-icon" src="<?php echo get_public_url('images/notifications.svg'); ?>" alt="Notifications Bell">
-                    <li>
-                        <a href="<?php echo get_public_url('/pages/profile-page.php'); ?>" title="View User Profile"><img class="img-container" src="<?php echo get_public_url('images/dave-koo-profile-image.jpg'); ?>" alt=""></a>
+                    <li class="user">
+                        <a href="<?php echo get_public_url('/pages/profile-page.php'); ?>" title="View User Profile"><img class="img-container" src="<?php echo get_public_url('images/dave-koo-profile-image.jpg'); ?>" alt=""><img class="drop-icon" src="https://img.icons8.com/material/256/expand-arrow.png" alt=""></a>
                     </li>
-                    <li class="username"><a href="<?php echo get_public_url('/pages/profile-page.php'); ?>">Dave Koo<img class="drop-icon" src="https://img.icons8.com/material/256/expand-arrow.png" alt=""></a></li>
                 </ul>
             </nav>
         </div>
