2cf894d85cd2a0e5227b963419af6f2e4ee4e691
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-04-12 14:49:02 -0700

    cache busting css

--- public/partials/global/head.php
+++ public/partials/global/head.php
@@ -3,6 +3,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forward - Meet and Connect with Local Mentors</title>
-    <link rel="stylesheet" href="<?php echo get_public_url('/styles/css/main.min.css'); ?>?v=1.1">
+    <link rel="stylesheet" href="<?php echo get_public_url('/styles/css/main.min.css'); ?>?v=1.3">
     <script src="https://unpkg.com/feather-icons"></script>
 </head>
