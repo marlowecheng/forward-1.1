b6d1303fd15c8a9f5933ccafafebd3f790e393b6
Kayla <kayla.kt.tran@gmail.com>
2023-04-13 22:38:29 -0700

    Side-Nav Revision
    Updated Side-Nav to Wireframe Look
    Added in Home.svg and changed all icons

--- public/partials/components/side-nav.php
+++ public/partials/components/side-nav.php
@@ -1,13 +1,22 @@
 <aside class="side-nav">
-        <img class="profile-image" src="<?php echo get_public_url('images/dave-koo-profile-image.jpg'); ?>" alt="">
-        <div class="nav-links">
-            <ul>
-                <li><img class="nav-icon" src="<?php echo get_public_url('images/home-svg.svg'); ?>" alt=""><a href="<?php echo get_public_url('/index.php'); ?>" title="Go to Home">Home</a></li>
-                <li><img class="nav-icon" src="<?php echo get_public_url('images/inbox-svg.svg'); ?>" alt=""><a href="#" title="Go to Inbox">Inbox</a></li>
-                <li><img class="nav-icon" src="<?php echo get_public_url('images/calendar-svg.svg'); ?>" alt=""><a href="#" title="See Your Calendar">Calendar</a></li>
-                <li><img class="nav-icon" src="<?php echo get_public_url('images/mail-svg.svg'); ?>" alt=""><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="View Your List of Projects">My Projects</a></li>
-                <li><img class="nav-icon" src="<?php echo get_public_url('images/building-svg.svg'); ?>" alt=""><a href="#" title="View Your Communities">My Community</a></li>
-                <li><img class="nav-icon" src="<?php echo get_public_url('images/kebab-svg.svg'); ?>" alt=""><a href="#" title="More options">More</a></li>
-            </ul>
+    <div class="profile-container">
+        <div class="profile-image-container">
+            <img class="profile-image" src="<?php echo get_public_url('images/dave-koo-profile-image.jpg'); ?>" alt="">
         </div>
+        <div class="profile-info">
+            <p class="txt-xl">Dave Koo</p>
+            <a href="#" title="View Profile">View Profile</a>
+        </div>
+    </div>
+
+    <div class="nav-links">
+        <ul>
+            <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/home.svg'); ?>" alt=""><a href="<?php echo get_public_url('/index.php'); ?>" title="Go to Home">Home</a></li>
+            <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/inbox.svg'); ?>" alt=""><a href="#" title="Go to Inbox">Inbox</a></li>
+            <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/calendar.svg'); ?>" alt=""><a href="#" title="See Your Calendar">Calendar</a></li>
+            <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/folder.svg'); ?>" alt=""><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="View Your List of Projects">My Projects</a></li>
+            <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/users.svg'); ?>" alt=""><a href="#" title="View Your Communities">My Community</a></li>
+            <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/more-horizontal.svg'); ?>" alt=""><a href="#" title="More options">More</a></li>
+        </ul>
+    </div>
 </aside>
