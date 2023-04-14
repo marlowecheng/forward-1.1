cf52b762dc015f64d006cc22972a8c40a406791d
Kayla <kayla.kt.tran@gmail.com>
2023-04-13 22:07:47 -0700

    Profile Page Revised
    Updated Profile Page Card Containers to match wireframes
    Added hover transition
    Changed spacing of content in card containers.

--- public/pages/profile-page.php
+++ public/pages/profile-page.php
@@ -16,8 +16,8 @@
                 <h1>Hiya, Dave!</h1>
                 <section class="profile-card">
                     <div class="card-information">
-                        <h2>Let's Find a Mentor</h2>
-                        <p>Take our survey and we'll help you find a compatible mentor near you</p>
+                        <h2>Let's Find a Mentor!</h2>
+                        <p class="txt-lg">Take our survey and we'll help you find a compatible mentor near you</p>
                         <a href="<?php echo get_public_url('/pages/survey.php'); ?>">Go to Survey &rarr;</a>
                     </div>
                     <img class="card-thumb" src="<?php echo get_public_url('/images/find-a-mentor.jpg'); ?>" alt="">
@@ -25,7 +25,7 @@
                 <section class="profile-card">
                     <div class="card-information">
                         <h2>Find a Community!</h2>
-                        <p>Join a community and work together on your goals.</p>
+                        <p class="txt-lg">Join a community and work together on your goals.</p>
                         <a href="#">Go to Community Hub &rarr;</a>
                     </div>
                     <img class="card-thumb" src="<?php echo get_public_url('/images/find-a-community.jpg'); ?>" alt="">
