217a850bc7ba49a0f23410008e4e0424fc46e408
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-04-14 18:29:10 -0700

    #2 added nav link to community icon

--- public/partials/components/side-nav.php
+++ public/partials/components/side-nav.php
@@ -15,7 +15,7 @@
             <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/inbox.svg'); ?>" alt="Inbox Icon"><a href="#" title="Go to Inbox">Inbox</a></li>
             <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/calendar.svg'); ?>" alt="Calendar Icon"><a href="#" title="See Your Calendar">Calendar</a></li>
             <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/folder.svg'); ?>" alt="Folder Icon"><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="View Your List of Projects">My Projects</a></li>
-            <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/users.svg'); ?>" alt="Community Icon"><a href="#" title="View Your Communities">My Community</a></li>
+            <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/users.svg'); ?>" alt="Community Icon"><a href="<?php echo get_public_url('/pages/community-hub.php'); ?>" title="View Your Communities">My Community</a></li>
             <li><img class="nav-icon" src="<?php echo get_public_url('images/Icons/more-horizontal.svg'); ?>" alt="More Icon"><a href="#" title="More options">More</a></li>
         </ul>
     </div>
